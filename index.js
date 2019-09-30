f2f38c746a96e711315571ab55503ec8b553e3bf
ireps <kprg556@astrazeneca.net>
2019-09-30 22:33:01 +0530

    QnA Bot

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const restify = require('restify');
 const { BotFrameworkAdapter } = require('botbuilder');
 
 // This bot's main dialog.
-const { EchoBot } = require('./bot');
+// const { EchoBot } = require('./bot');
+const { QnABot } = require('./bots/QnABot');
 
 // Import required bot configuration.
 const ENV_FILE = path.join(__dirname, '.env');
@@ -35,19 +36,30 @@ const adapter = new BotFrameworkAdapter({
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
+    // This check writes out errors to console log
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
     console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
     await context.sendActivity(`Oops. Something went wrong!`);
 };
 
 // Create the main dialog.
-const bot = new EchoBot();
+// const bot = new EchoBot();
+const bot = new QnABot();
 
-// Listen for incoming requests.
+// Create HTTP server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+});
+
+// Listen for incoming activities and route them to your bot main dialog.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
-        await bot.run(context);
+    // Route received a request to adapter for processing
+    adapter.processActivity(req, res, async (turnContext) => {
+        // route to bot activity handler.
+        await bot.run(turnContext);
     });
 });
