f59dbef4b2043352be02d7289847cd6b50f73221
ireps <kprg556@astrazeneca.net>
2019-10-01 10:35:28 +0530

    fixed HTTP Server duplicate error

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const { QnABot } = require('./bots/QnABot');
 const ENV_FILE = path.join(__dirname, '.env');
 dotenv.config({ path: ENV_FILE });
 
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open the emulator select "Open Bot"`);
-});
+// // Create HTTP server
+// const server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, () => {
+//     console.log(`\n${ server.name } listening to ${ server.url }`);
+//     console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+//     console.log(`\nTo talk to your bot, open the emulator select "Open Bot"`);
+// });
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about how bots work.
